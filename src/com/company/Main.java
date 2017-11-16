f0b42f7c23ec6a3ec7e0e399a2ac0782a267bcba
ch983 <cjhainley@gmail.com>
2017-11-16 08:49:46 -0500

    I hope the average is right

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,7 @@ public class Main {
         }
         scanner.close();
         for (int i=0; i<schoolInfo.length; i++) {
-            if (schoolInfo != null) {
+            if (schoolInfo[i] != null) {
                 Scanner scanner2 = new Scanner(schoolInfo[i]);
                 int x;
                 for(x=0; scanner2.hasNext(); x++) {
@@ -30,23 +30,52 @@ public class Main {
             }
         }
         for (int i=1; i<schoolInfo.length; i++) {
-            Scanner scanner3 = new Scanner(schoolInfo[i]);
             if(schoolInfo[i] != null){
+                Scanner scanner3 = new Scanner(schoolInfo[i]);
+                scanner3.useDelimiter("\t");
+                scanner3.next();
                 String name = scanner3.next();
-                for(int j = 0; j < 18; j++) {
+                for(int j = 0; j < 16; j++) {
                     scanner3.next();
                 }
-                int math = scanner3.nextInt();
-                int reading = scanner3.nextInt();
-                int writing = scanner3.nextInt();
+                int math = 0;
+                int reading = 0;
+                int writing = 0;
+                if (scanner3.hasNextInt()){
+                    math = scanner3.nextInt();
+                }
+                if (scanner3.hasNextInt()) {
+                    reading = scanner3.nextInt();
+                }
+                if (scanner3.hasNextInt()) {
+                    writing = scanner3.nextInt();
+                }
                 schools[i] = new School(name, math, reading, writing);
+               // System.out.println(math);
             }
 
         }
         for (int i= 0; i<schools.length; i++){
             if (schools[i] != null){
-                System.out.println(schools[i].name + ", " + schools[i].getAverage());;
+                //System.out.println(schools[i].name + ", " + schools[i].getAverage());;
+
             }
         }
+        System.out.println(NYAverage(schools));
+    }
+    public static int NYAverage(School[]schools){
+        int count= 0;
+        int sum= 0;
+        for (int i = 0; i<schools.length; i++){
+            if (schools[i] != null){
+                if (schools[i].getAverage() != 0) {
+                    sum += schools[i].getAverage();
+                    count++;
+                }
+            }
+
+        }
+        return sum/count;
+
     }
 }
